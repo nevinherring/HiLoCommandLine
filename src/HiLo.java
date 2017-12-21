21fd0c57caef4d76991085701bb131b1713c6550
Nevin Herring <nevin.herring@gmail.com>
2017-12-20 22:29:38 -0600

    add do while to continue game

--- src/HiLo.java
+++ src/HiLo.java
@@ -7,27 +7,40 @@ public class HiLo {
 		// get ready for the player to user the keyboard
 		Scanner scan = new Scanner(System.in);
 		
-		// create a random number from 1-100
-		int theNumber = (int)(Math.random()*100 + 1);
+		//play again string
+		String playAgain = "";
 		
-		int guess = 0;
+		// game loop
+		do {
+			// create a random number from 1-100
+			int theNumber = (int)(Math.random()*100 + 1);
+			
+			int guess = 0;
+			
+			while ( guess != theNumber ) {
+				System.out.println("Guess a number between 1 and 100:");
+				
+				// get the user's guess
+				guess = scan.nextInt();
+				
+				if (guess < theNumber) {
+					System.out.println(guess + " is too low.  Try again.");
+				}else if(guess > theNumber) {
+					System.out.println(guess + " is too high.  Try again.");
+				}else {
+					System.out.println(guess + " is correct!");
+				}
+				
+			} // end of while loop for guessing
+			
+			// ask for 'y' to play again
+ 			System.out.println("Would you like to play again (y/n)?");
+		    playAgain = scan.next();
+ 
+		}  while(playAgain.equalsIgnoreCase("y"));	
+		// thank the user for playing
 		
-		while ( guess != theNumber ) {
-			System.out.println("Guess a number between 1 and 100:");
-			
-			// get the user's guess
-			guess = scan.nextInt();
-			
-			if (guess < theNumber) {
-				System.out.println(guess + " is too low.  Try again.");
-			}else if(guess > theNumber) {
-				System.out.println(guess + " is too high.  Try again.");
-			}else {
-				System.out.println(guess + " is correct!");
-			}
-			
-		}
-
+		System.out.println("Thank you for playing!  Goodbye.");
 	}
 
 }
